a8ae09ef10edb67727c836de89001948d99090dd
calaf <117057567+ca1af@users.noreply.github.com>
2024-12-04 14:25:54 +0900

    init

---
+++ src/main/kotlin/com/example/prdeliver/PrDeliverApplication.kt
@@ -0,0 +1,11 @@
+package com.example.prdeliver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PrDeliverApplication
+
+fun main(args: Array<String>) {
+    runApplication<PrDeliverApplication>(*args)
+}
